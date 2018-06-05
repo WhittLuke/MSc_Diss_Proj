131d8076fbdefd9baa7d2326fb0829c227cf3c9e
WhittLuke <luke.whitt242@gmail.com>
2018-06-05 18:19:20 +0100

    Initial upload of project code files

---
+++ LondonTrafficVisualisation/dataVisApp.js
@@ -0,0 +1,27 @@
+
+// Let's try and get access to the elements from the user search
+
+// Global variables
+var borough = "";
+var year = "";
+var vehicle = "";
+var road_type = "";
+
+
+function getBorough(){
+    borough = document.getElementById("selBorough");
+    console.log(borough);
+}
+
+// function getUsersSearch(){
+//
+//     borough = document.getElementById("selBorough");
+//     year = document.getElementById("selYear");
+//     vehicle = document.getElementById("selVehicle");
+//     road_type = document.getElementById("selRoad");
+//
+//     console.log(borough);
+//     console.log(year);
+//     console.log(vehicle);
+//     console.log(road_type);
+// }
