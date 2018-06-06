634a0a26ac9b2991af2767589624d382491fab0d
WhittLuke <luke.whitt242@gmail.com>
2018-06-06 13:31:27 +0100

    Uploading some minor changes to the layout and removal of the Submit button

--- LondonTrafficVisualisation/dataVisApp.js
+++ LondonTrafficVisualisation/dataVisApp.js
@@ -1,13 +1,12 @@
 
-// Let's try and get access to the elements from the user search
-
-// Global variables
+// Global variables go here!!
 var borough = "";
 var year = "";
 var vehicle = "";
 var road_type = "";
 
 
+// TODO:Create functions for accessing the values associated with the list boxes
 function getBorough(){
     borough = document.getElementById("selBoroughs").value;
     console.log("Borough: " + borough);
@@ -30,3 +29,11 @@ function getRoadType(){
     road_type = document.getElementById("selRoad").value;
     console.log("Road Type: " + road_type);
 }
+
+
+
+// TODO: Create one or two functions for reading in the data requested for by the user
+// TODO: Will probably need to convert the coordinated into the GIS values.
+
+
+// TODO: Then we can work on creating the map here
